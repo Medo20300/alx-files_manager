07c93a2b5e1622f6e08bb7928ec860d58d3970c5
Yara-ib <yr.ap.23@gmail.com>
2024-09-30 21:15:38 +0300

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,41 @@
+const { createClient } = require('redis');
+const { promisify } = require('util');
+
+let connectionCheck = false;
+
+class RedisClient {
+  constructor() {
+    this.client = createClient()
+      .on('error', (error) => {
+        console.error(error);
+      })
+      .on('connect', () => {
+        connectionCheck = true;
+      });
+  }
+
+  async isAlive() {
+    if (!connectionCheck) {
+      await new Promise((resolve) => {
+        this.client.once('connect', resolve);
+      });
+    }
+    return connectionCheck;
+  }
+
+  async get(key) {
+    const promise = promisify(this.client.get).bind(this.client);
+    return await promise(key);
+  }
+
+  async set(key, value, duration) {
+    await this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
